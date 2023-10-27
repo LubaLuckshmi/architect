199007562a90832fc0318519381100cde0836577
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-10-27 14:17:27 +0300

    Семинар 7

---
+++ src/main/java/seminar7/Main.java
@@ -0,0 +1,7 @@
+package seminar7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Оформили UseCase для Интернет-магазина запчастей пылесосов");
+    }
+}
